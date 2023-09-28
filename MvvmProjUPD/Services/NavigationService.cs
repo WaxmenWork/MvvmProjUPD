ef46ab82ef5506180f638410aa02a4c7640cf8bb
AleksandrovichPA <AleksandrovichPA@202D-9>
2023-09-28 16:00:37 +0700

    Добавьте файлы проекта.

---
+++ MvvmProjUPD/Services/NavigationService.cs
@@ -0,0 +1,42 @@
+﻿using MvvmProjUPD.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmProjUPD.Services
+{
+    public interface INavigationService
+    {
+        ViewModelBase CurrentView { get; }
+        void NavigateTo<T>() where T : ViewModelBase;
+    }
+
+    public class NavigationService : ObservableObject, INavigationService
+    {
+        private readonly Func<Type, ViewModelBase> _viewModelFactory;
+        private ViewModelBase _currentView;
+
+        public ViewModelBase CurrentView
+        {
+            get => _currentView;
+            private set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NavigationService(Func<Type, ViewModelBase> viewModelFactory)
+        {
+            _viewModelFactory = viewModelFactory;
+        }
+
+        public void NavigateTo<TViewModelBase>() where TViewModelBase : ViewModelBase
+        {
+            ViewModelBase viewModelBase = _viewModelFactory.Invoke(typeof(TViewModelBase));
+            CurrentView = viewModelBase;
+        }
+    }
+}
